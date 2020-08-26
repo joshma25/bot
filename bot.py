d8cc71c305ca60f0e569601b059bff7c0afc676b
joshma25 <45340339+joshma25@users.noreply.github.com>
2020-08-25 20:05:59 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
-import random
+import youtube_dl
+import os
 from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from os import system
 
 bot = commands.Bot(command_prefix='.')
 
@@ -19,7 +23,6 @@ async def on_member_remove(member):
 @bot.command()
 async def hi(ctx):
     await ctx.send('Wassup baby')
-    await bot.close()
 
 @bot.command()
 async def magic8ball(ctx, *, question):
@@ -57,7 +60,43 @@ async def magicconch(ctx, *, question):
 
 @bot.command()
 async def commands(ctx):
-    await ctx.send("Commands: .hi \n .magic8ball *question* \n .magicconch *question*")
-    await bot.close()
+    await ctx.send("Commands: .hi \n .magic8ball *question* \n .magicconch *question*").j
 
-bot.run('NzE4NjI2Mzg5OTg4ODY4MTM3.Xtrngw.PdakAJlzDzZq1mZuKdUKRp2_W7A')
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    await voice.disconnect()
+
+@bot.command()
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    if song_there:
+        os.remove("song.mp3")
+    await ctx.send("beep bop boop boop")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, 'song.mp3')
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.volume = 100
+    voice.is_playing()
+
+
+
+bot.run('#code goes here')
